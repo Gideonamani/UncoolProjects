b7cf303957483bc510274d2501f3822d0d6f8884
Gideonamani <gmarress@gmail.com>
2017-05-14 15:18:17 +0300

    Add the email verification code

--- 005-firebase-sign-in-and-email-verification/script.js
+++ 005-firebase-sign-in-and-email-verification/script.js
@@ -27,12 +27,16 @@ firebase.initializeApp(config);
 // });
 
 
-// // Signing OUT a user to your firebase app
-// firebase.auth().signOut().then(function() {
-//   // Sign-out successful.
-// }).catch(function(error) {
-//   // An error happened.
-// });
+// Signing OUT a user to your firebase app
+function signOut(){
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    alert("Signed out.");
+  }).catch(function(error) {
+    // An error happened.
+    alert("Sign OUT Error!!");
+  });
+}
 
 function showSignInPage(){
   document.querySelector("#landing").style.display = "none";
@@ -71,14 +75,35 @@ function signInUserToFirebase(email, password){
   var errMsgP = document.querySelector("#sign-in-page p.error-msg");
   errMsgDiv.style.display = "none";
   console.log(errMsgDiv);
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    errMsgDiv.style.display = "block";
-    errMsgP.textContent = "ERROR: " + errorMessage;
-    console.log("Sign IN Error. \n Error:", errorCode, " \n The Message is:", errorMessage);
-  });
+  firebase.auth().signInWithEmailAndPassword(email, password).
+      then(function(userData){
+        console.log("About to send the User Data to DB!");
+        var ref = firebase.database().ref("userData/"+userData.uid);
+        var data = {
+          email: userData.email,
+          emailVerified: userData.emailVerified,
+          uid: userData.uid
+        };
+        ref.set(data).then(function(){
+            showProfilePage();
+          })
+          .catch(function(error) {
+            // Handle error writing to the database
+            displayErrorMsg(error, errMsgDiv, errMsgP);
+          });
+      })
+      .catch(function(error) {
+        // Handle Errors here for Sign In.
+        displayErrorMsg(error, errMsgDiv, errMsgP);
+      });
+}
+
+function displayErrorMsg(error, errMsgDiv, errMsgP){
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  errMsgDiv.style.display = "block";
+  errMsgP.textContent = "ERROR: " + errorMessage;
+  console.log("Sign IN Error. \n Error:", errorCode, " \n The Message is:", errorMessage);
 }
 
 function checkSignUp(){
@@ -104,36 +129,48 @@ function signUpUserToFirebase(email, password){
   var errMsgP = document.querySelector("#sign-up-page p.error-msg");
   errMsgDiv.style.display = "none";
   // Signing UP a user to your firebase app
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    errMsgDiv.style.display = "block";
-    errMsgP.textContent = "ERROR: " + errorMessage;
-    console.log("Sign UP Error. \n Error:", errorCode, " \n The Message is:", errorMessage);
-  });
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(function(userData){
+        console.log(userData);
+        clearInputs(document.querySelector("#sign-up-page"));
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        errMsgDiv.style.display = "block";
+        errMsgP.textContent = "ERROR: " + errorMessage;
+        console.log("Sign UP Error. \n Error:", errorCode, " \n The Message is:", errorMessage);
+      });
 }
 
 window.onload = function(){
+  checkForUser()
   listenForFirstKeyUpOnCPI();
+  listenForLikes();
+  // showHomePage();
 }
 
 // function to listen for the first key up event on the Confirm Password Input
 function listenForFirstKeyUpOnCPI(){
-  document.querySelector("#sign-up-page #confirm-password").addEventListener("keyup", function(e){
-    var passConfirmInput = e.target;
-    var passInput = document.querySelector("#sign-up-page #password");
-    checkIfPasswordsMatch(passInput, passConfirmInput);
-    listenForKeyUpOnPassInput();
-  })
+  var passConfirmInput = document.querySelector("#sign-up-page #confirm-password");
+  if(passConfirmInput){
+    passConfirmInput.addEventListener("keyup", function(e){
+      var passInput = document.querySelector("#sign-up-page #password");
+      checkIfPasswordsMatch(passInput, passConfirmInput);
+      listenForKeyUpOnPassInput();
+    })
+  }
 }
 
 function listenForKeyUpOnPassInput(){
-  document.querySelector("#sign-up-page #password").addEventListener("keyup", function(e){
-    var passInput = e.target;
-    var passConfirmInput = document.querySelector("#sign-up-page #confirm-password");
-    checkIfPasswordsMatch(passInput, passConfirmInput);
-  })
+  var passInput = document.querySelector("#sign-up-page #password");
+  if(passInput){  
+    passInput.addEventListener("keyup", function(e){
+      var passConfirmInput = document.querySelector("#sign-up-page #confirm-password");
+      checkIfPasswordsMatch(passInput, passConfirmInput);
+    })
+  }
 }
 
 function checkIfPasswordsMatch(passInput, passConfirmInput){
@@ -185,36 +222,132 @@ randomizeStats = function(){
   })
 }
 
-document.querySelectorAll(".bottom-block").forEach(function(el){
-  // Add event listeners to all the bottom blocks (ie, like, bookmark and comment)
-  el.addEventListener('click', function(){
 
-    // whenever we click we want to give feedback with a slight light gray blink
-    // that this area has been clicked and the action (like or bookmark) has
-    // been engaged
-    el.classList.add("bg-gray");
-    setTimeout(function(){
-      el.classList.remove("bg-gray");
-    }, 100);
+function listenForLikes(){
+  document.querySelectorAll(".bottom-block").forEach(function(el){
+    // Add event listeners to all the bottom blocks (ie, like, bookmark and comment)
+    el.addEventListener('click', function(){
 
-    // Then we change the colour of the icon to give a postive feedback
-    // also we adjust the stats number up or down by one.
-    var iconContainer = el.querySelector(".icon-container");
-    var statsTextP = el.querySelector(".stats-text p");
-    var currentCount = Number(statsTextP.dataset.engagementCounter);
-    var updatedCount;
+      // whenever we click we want to give feedback with a slight light gray blink
+      // that this area has been clicked and the action (like or bookmark) has
+      // been engaged
+      el.classList.add("bg-gray");
+      setTimeout(function(){
+        el.classList.remove("bg-gray");
+      }, 100);
 
-    if( !iconContainer.className.includes("engaged") ){
-      updatedCount = currentCount+1;
-      statsTextP.classList.add("green-highlight");
+      // Then we change the colour of the icon to give a postive feedback
+      // also we adjust the stats number up or down by one.
+      var iconContainer = el.querySelector(".icon-container");
+      var statsTextP = el.querySelector(".stats-text p");
+      var currentCount = Number(statsTextP.dataset.engagementCounter);
+      var updatedCount;
+
+      if( !iconContainer.className.includes("engaged") ){
+        updatedCount = currentCount+1;
+        statsTextP.classList.add("green-highlight");
+      }else{
+        updatedCount = currentCount-1;
+        statsTextP.classList.remove("green-highlight");
+      }
+
+      statsTextP.dataset.engagementCounter = updatedCount;
+      statsTextP.textContent = String( shortenLargeNumber(updatedCount, 1) );
+      iconContainer.classList.toggle("engaged");
+      // console.log("clicked", updatedCount);
+    })
+  })
+}
+
+function checkForUser(){
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      removeLoadingScreen();
+      showProfilePage();
+      getProfileData();
+    } else {
+      // No user is signed in.
+      removeLoadingScreen();
+      showHomePage();
+    }
+  });
+}
+
+
+// Profile Functions
+function getProfileData(){
+  var currentUser = firebase.auth().currentUser;
+  // var ref = firebase.database().ref().child("userData").child(authUser.uid);
+  var displayNameH2 = document.querySelector("#profile-display-name");
+  var emailP = document.querySelector("#profile-email");
+  // ref.once("value").then( function(snap){
+    // if  user hasn't set the username yet use the email.
+    if(currentUser.displayName){
+      displayNameH2.textContent = currentUser.displayName;
     }else{
-      updatedCount = currentCount-1;
-      statsTextP.classList.remove("green-highlight");
+      displayNameH2.textContent = currentUser.email;
     }
 
-    statsTextP.dataset.engagementCounter = updatedCount;
-    statsTextP.textContent = String( shortenLargeNumber(updatedCount, 1) );
-    iconContainer.classList.toggle("engaged");
-    // console.log("clicked", updatedCount);
-  })
-})
+    //if the email is unverified
+    if(!currentUser.emailVerified){
+      document.querySelector("#profile-email-div").classList.add("email-unverified");
+      document.querySelector("#verify-email-button").hidden = false;
+    }
+
+    emailP.title = currentUser.email;
+    emailP.textContent = currentUser.email;
+    console.log(currentUser);
+  // });
+}
+
+function verifyEmail(){
+  var user = firebase.auth().currentUser;
+  user.sendEmailVerification().then(function() {
+    // Email sent.
+    document.querySelector("#verify-email-button").hidden = true;
+    document.querySelector("#verification-sent-info").hidden = false;
+    console.log("Email has been sent.");
+  }).catch( function(error) {
+    // An error happened.
+    document.querySelector("#verification-sent-info").textContent = error.message;
+  });
+}
+
+function refresh(){
+  window.location = "";
+}
+
+// Profile Setting Functions
+
+
+
+// General Functions
+function removeLoadingScreen(){
+  document.querySelector("#loading-screen").style.display = "none";
+}
+
+function showHomePage(){
+  document.querySelector("#profile-page").hidden = true;
+  document.querySelector("#profile-settings-page").hidden = true;
+  document.querySelector("#home-page").hidden = false;
+}
+
+function showProfilePage(){
+  document.querySelector("#profile-settings-page").hidden = true;
+  document.querySelector("#home-page").hidden = true;
+  document.querySelector("#profile-page").hidden = false;
+}
+
+function showProfileSettingsPage(){
+  document.querySelector("#home-page").hidden = true;
+  document.querySelector("#profile-page").hidden = true;
+  document.querySelector("#profile-settings-page").hidden = false;
+}
+
+function clearInputs(domSection){
+  var inputs = domSection.querySelectorAll("input");
+  inputs.forEach(function(input){
+    input.value = "";
+  });
+}
